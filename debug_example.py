8193f3f65f5284f9dd941ec9d9e579d7af5ab13a
Anthony <anthonyqmle@gmail.com>
2020-10-03 08:39:15 -0400

    Debug Example Code

---
+++ debug_example.py
@@ -0,0 +1,15 @@
+def even_check(a):
+    if (a%2==0):
+        return True
+    else:
+        return False
+
+def square_number(a):
+    b = a**2
+    print(str(b))
+
+m = 2
+n = m*2
+square_number(n)
+k=3
+
